30c6ad40df1a84b312e9c03b3cd849dd7fe157f5
Arlen Cuss <ar@len.me>
2012-09-07 23:03:57 +1000

    Read strings.

--- lib/reader.rb
+++ lib/reader.rb
@@ -3,6 +3,7 @@
 class Reader
   class UnexpectedCharacterError < StandardError; end
   class TrailingDataError < StandardError; end
+  class EndOfDataError < StandardError; end
 
   def self.read(input)
     new(input).lex
@@ -20,6 +21,8 @@ class Reader
         number
       when SYMBOL
         symbol
+      when STRING_OPEN
+        string
       else
         raise UnexpectedCharacterError, "#{peek} in #lex"
       end
@@ -41,6 +44,46 @@ class Reader
     slurp(SYMBOL).intern
   end
 
+  def string
+    s = ""
+    t = consume
+    while true
+      c = consume
+
+      if c.nil?
+        raise EndOfDataError, "in string, got: #{s}"
+      end
+
+      if c == t
+        break
+      end
+
+      if c == ?\\
+        c = consume
+
+        case c
+        when nil
+          raise EndOfDataError, "in escaped string, got: #{s}"
+        when /[abefnrstv]/
+          c = {?a => ?\a,
+               ?b => ?\b,
+               ?e => ?\e,
+               ?f => ?\f,
+               ?n => ?\n,
+               ?r => ?\r,
+               ?s => ?\s,
+               ?t => ?\t,
+               ?v => ?\v}[c]
+        else
+          # Just leave it be.
+        end
+      end
+
+      s += c
+    end
+    s
+  end
+
   def slurp re
     @src =~ re
     @n += $&.length
@@ -51,8 +94,15 @@ class Reader
     @src[@n]
   end
 
+  def consume
+    c = peek
+    @n += 1
+    c
+  end
+
   NUMBER = /^[0-9][0-9_]*/
-  SYMBOL = /^[a-zA-Z0-9\-_!\?]+/
+  SYMBOL = /^[a-zA-Z0-9\-_!\?\*\/]+/
+  STRING_OPEN = /^['"]/
 end
 
 # vim: set sw=2 et cc=80:
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -16,6 +16,23 @@ describe Reader do
 
     it "should read symbols" do
       Reader.read("loki").should eq(:loki)
+      Reader.read("/").should eq(:/)
+      Reader.read("wah?").should eq(:wah?)
+      Reader.read("!ruby!").should eq(:"!ruby!")
+    end
+
+    describe "strings" do
+      it "should read plain strings" do
+        Reader.read("\"akashi yo\"").should eq("akashi yo")
+        Reader.read("\"akashi \n woah!\"").should eq("akashi \n woah!")
+      end
+
+      it "should read escape sequences" do
+        Reader.read("\"here \\\" goes\"").should eq("here \" goes")
+        Reader.read("\"here \\\\ goes\"").should eq("here \\ goes")
+        Reader.read("\"\\a\\b\\e\\f\\n\\r\"").should eq("\a\b\e\f\n\r")
+        Reader.read("\"\\s\\t\\v\"").should eq("\s\t\v")
+      end
     end
   end
 end
