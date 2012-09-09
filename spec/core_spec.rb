10d1e7c7991de68bd1554fdee1d2b24c45eeaafa
Arlen Cuss <ar@len.me>
2012-09-09 20:35:30 +1000

    Refactor core specs.

--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -2,22 +2,36 @@
 require 'spec_helper'
 require 'rl'
 
-# TODO: possibly move?  Also maybe work out a better way to test.
-
-describe RL::Keyword do
+describe [RL::Keyword, RL::Macro, RL::Builtin] do
   describe "the constructor" do
-    it "should return a new keyword" do
-      RL::Keyword.new(:abc).should be_an_instance_of RL::Keyword
+    it "should return a new wrapper" do
+      described_class.each do |klass|
+        klass.new(:abc).should be_an_instance_of klass
+      end
     end
 
     it "should function with the alternate form" do
-      RL::Keyword[:aoeu].should eq RL::Keyword.new(:aoeu)
+      described_class.each do |klass|
+        klass[:aoeu].should eq klass.new(:aoeu)
+      end
     end
   end
 
-  describe "keyword equality" do
-    it "should be true for two keyword with the same underlying symbol" do
-      RL::Keyword.new(:xyz).should eq RL::Keyword.new(:xyz)
+  describe "equality" do
+    it "should be true for two wrappers with the same underlying object" do
+      described_class.each do |klass|
+        klass.new(:xyz).should eq klass.new(:xyz)
+      end
+    end
+  end
+
+  describe "the inner getter" do
+    it "should return the object passed in" do
+      described_class.each do |klass|
+        klass.new(:boohoo).inner.should eq :boohoo
+        l = lambda {}
+        klass.new(l).inner.should eq l
+      end
     end
   end
 end
