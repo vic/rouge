0e8671df6754385d5f9b3f19ed619dd8143249b6
Arlen Cuss <ar@len.me>
2012-09-09 14:22:23 +1000

    Built-ins.  Need more clear thinking.

--- lib/rl/eval.rb
+++ lib/rl/eval.rb
@@ -4,6 +4,15 @@ require 'rl/core'
 module RL::Eval
   require 'rl/eval/context'
 
+  module Builtins
+    def self.let(context, bindings, *body)
+      context = RL::Eval::Context.new context
+      bindings.each_slice(2) do |k, v|
+        context.set_here k, RL::Eval.eval(v, context)
+      end
+    end
+  end
+
   def self.eval(form, context)
     case form
     when Symbol
@@ -11,7 +20,7 @@ module RL::Eval
     when Array
       fun = eval form[0], context
       if fun.is_a? RL::Macro
-        eval fun.lamda.call(*form[1..-1]), context
+        eval fun.call(context, *form[1..-1]), context
       else
         fun.call *form[1..-1].map {|f| eval f, context}
       end
