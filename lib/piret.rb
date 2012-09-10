a0e5c5c14f5a727affb9a1ba1ae5d03c2e8ea01c
Arlen Cuss <ar@len.me>
2012-09-11 08:37:43 +1000

    Add namespace, REPL helpers.

--- lib/piret.rb
+++ lib/piret.rb
@@ -5,17 +5,48 @@ module Piret
   require 'piret/reader'
   require 'piret/printer'
   require 'piret/eval'
+end
 
-  def self.read(input)
+class << Piret
+  def read(input)
     Piret::Reader.read input
   end
 
-  def self.eval(context, *forms)
+  def eval(context, *forms)
     Piret::Eval.eval context, *forms
   end
 
-  def self.print(form)
+  def print(form)
     Piret::Printer.print form
+  end
+
+  def [](ns)
+    Piret::Eval::Namespace[ns]
+  end
+
+  def repl
+    ns = Piret[:user]
+    ns.refers Piret[:piret]
+    ns.refers Piret[:ruby]
+    context = Piret::Eval::Context.new ns
+
+    require 'readline'
+    while true
+      input = Readline.readline("#{context.ns.name}=> ", true)
+      if input.nil?
+        STDOUT.print "\n"
+        break
+      end
+
+      begin
+        form = Piret.read(input)
+        result = Piret.eval(context, form)
+        STDOUT.puts Piret.print(result)
+      rescue => e
+        STDOUT.puts "!! #{e.class}: #{e.message}"
+        STDOUT.puts "#{e.backtrace.join "\n"}"
+      end
+    end
   end
 end
 
